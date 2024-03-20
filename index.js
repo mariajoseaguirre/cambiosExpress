fb5f6bb4148632fb3feec16ff742b8533534e236
MariaJose <mariajoseaguirre96@gmail.com>
2024-03-20 06:35:54 -0300

    Tercera Preentrega

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
 datosTransaccion = [];
 
+const clUsTasa = 948;
+const usClTasa = 1/948;
 
 function convertirDivisa (tipoDeCambio, cantidadDinero) {
-    if (tipoDeCambio == clUs.opcion) {
-        return cantidadDinero * clUs.tasaCambio;
+    console.log('argumentos',tipoDeCambio, cantidadDinero)
+    if (tipoDeCambio == 'cl-Us') {
+        return cantidadDinero * clUsTasa;
     } else {
-        return cantidadDinero * usCl.tasaCambio;
+        return cantidadDinero * usClTasa;
     }
 }
 
-let nombreCliente = document.getElementById("nombre")
-let numIdentificacion =parseInt(document.getElementById("identificacion"))
-let cambioClUs = document.getElementById("cl-us")
-let cambioUsCl = document.getElementById ("us-cl")
-let cantidadDinero = document.getElementById("cantidad")
-let formulario = document.getElementById("formulario")
-
-
 formulario.addEventListener("submit", event=>{
     event.preventDefault()
+    let nombreCliente = document.getElementById("nombre")
+    let numIdentificacion = document.getElementById("identificacion")
+    let cantidadDinero = document.getElementById("cantidad")
+    let formulario = document.getElementById("formulario")
     if (nombreCliente.value == ''){
         const span = document.createElement("span")
         span.innerText = "Ingrese un nombre valido"
         formulario.appendChild(span)
     }
 
-    if (numIdentificacion.value ==  0) {
+    if ((parseInt(numIdentificacion.value) ==  0) || Number.isNaN(parseInt(numIdentificacion.value))){
         const span = document.createElement("span")
         span.innerText = "Ingrese numero de identificacion valido"
         formulario.appendChild(span)
     }
 
-    let divisaConvertida = convertirDivisa(tipoDeCambio, cantidadDinero);
- 
-    switch (tipoDeCambio) {
-      case "cambioClUs":
-        break;
-      case "cambioUsCl":
-        break;
-      default:
+    if ((parseInt(cantidadDinero.value) ==  0) || Number.isNaN(parseInt(cantidadDinero.value))){
         const span = document.createElement("span")
-        span.innerText = "Ingrese una opcion"
+        span.innerText = "Ingrese numero de identificacion valido"
         formulario.appendChild(span)
-        break;
     }
 
 
 
-    
-    let datosGuardados = {
-        nombreCliente: nombreCliente,
-        numIdentificacion:numIdentificacion,
-        cantidadDinero: cantidadDinero,
-        divisaConvertida: divisaConvertida,
-    }
+    let tipoDeCambio = document.querySelector('input[name="tipo-cambio"]:checked').value;
+
+
+
+    let divisaConvertida = convertirDivisa(tipoDeCambio, parseInt(cantidadDinero.value));
+    const span = document.createElement("span")
+    span.innerText = `El dinero a entregar es: ${divisaConvertida.toFixed(2)}`
+    formulario.appendChild(span)
+
 })
+
